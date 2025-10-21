c6fd4cbc0a648c0835198df535dff49225e4f474
whwhwjddn123 <whwhwjddn123@gmail.com>
2025-10-21 10:43:37 +0900

    first

---
+++ src/main/java/com/take/take_breath/TakeBreathApplication.java
@@ -0,0 +1,13 @@
+package com.take.take_breath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeBreathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeBreathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/take/take_breath/TakeBreathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.take.take_breath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeBreathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
